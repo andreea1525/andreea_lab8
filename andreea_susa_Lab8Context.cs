71dc57b73eb7fa23f22e665124978b4d01d0ccb4
andreea1525 <97646321+andreea1525@users.noreply.github.com>
2022-01-13 20:33:10 +0200

    Add files via upload

---
+++ andreea_susa_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab8.Models;
+
+namespace Lab8.Data
+{
+    public class Lab8Context : DbContext
+    {
+        public Lab8Context (DbContextOptions<Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Lab8.Models.Category> Category { get; set; }
+    }
+}
